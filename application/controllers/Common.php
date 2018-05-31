d6a77e786f0a5264d7b74c0ee9a288c4f05185c2
nameisshow <1965543973@qq.com>
2018-05-31 22:04:16 +0800

    view

--- application/controllers/Common.php
+++ application/controllers/Common.php
@@ -1,6 +1,7 @@
 <?php
 
 use Yaf\Controller_Abstract;
+use Yaf\Registry;
 
 /**
  * Created by PhpStorm.
@@ -12,6 +13,8 @@ class CommonController extends Controller_Abstract{
     public function commonAction()
     {
         dump('this is common');
+        $this->getView()->assign('viewpath',Registry::get('config')->commonViewPath);
         $this->getView()->assign('commonContent','来自common的变量');
+        $this->getView()->assign('page','common说当前页是1');
     }
 }
